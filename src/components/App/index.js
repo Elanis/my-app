78ef2631d53d561da3938f2485eaa05d07a88dce
Elanis <elanis@hotmail.com>
2022-05-23 16:31:07 +0200

    New component: MyComponent

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,12 @@
 import './index.css';
 
+import MyComponent from '../MyComponent';
+
 export default function App() {
 	return (
 		<div className="my-app">
-			Test
+			Test<br />
+			<MyComponent name={'world'} />
 		</div>
 	);
 }
