41b6675f8b3bbb0f04c941c414a8fdbfdc1e3249
Elanis <elanis@hotmail.com>
2022-06-13 09:10:54 +0200

    Added "Counter" component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,14 @@
 import './index.css';
 
+import Counter from '../Counter';
 import MyComponent from '../MyComponent';
 
 export default function App() {
 	return (
 		<div className="my-app">
 			Test<br />
-			<MyComponent name={'world'} />
+			<MyComponent name={'world'} /><br/>
+			<Counter />
 		</div>
 	);
 }
